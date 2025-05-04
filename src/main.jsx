a0b0a7c885d28209b50605745cf28360f08dc5dc
Ehsan Madadi <ehsanmadadi11@gmail.com>
2025-05-04 17:37:58 +0330

    more advanced way for yup error msgs-setLocale

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { setLocale } from 'yup'
 
+setLocale({
+  mixed:{
+    required: ({path})=>`الزامی است ${path} وارد کردن فیلد `
+  },
+  string :{
+    min : ({min , path})=>`کاراکتر است ${path} ${min} حداقل طول فیلد`
+  }
+})
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
